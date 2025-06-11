e35e63b5d7ef7843d1f07a8804c9840f4a13b3f8
itsandrewduh <drewtchau@yahoo.com>
2025-06-11 14:16:38 -0700

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,1 +1,12 @@
-
+#Importing SPY data
+pip install yfinance
+import yfinance as yf
+df = yf.download('SPY')
+#Getting SPY data
+def get_data():
+  df = yf.download(TICKER)
+  df.columns = df.columns.get_level_values(0)
+  TICKER = 'SPY'
+  FAST = 10
+  SLOW = 27
+  LOOKBACK = 10000
